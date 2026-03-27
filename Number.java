e4da7db3b7b6f382486c1269e5ad2823f896a189
Chainarin04 <684259042@webmail.npru.ac.th>
2026-03-27 12:17:34 +0700

    27-3-69

---
+++ Number.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Number {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        int count = 0, sum = 0, even = 0, odd = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        while (true) {
+            System.out.print("ป้อนตัวเลข (-1 เพื่อหยุด): ");
+            int n = kb.nextInt();
+
+            if (n == -1) {
+                break;
+            }
+
+            count++;
+            sum += n;
+
+            if (n > max)
+                max = n;
+            if (n < min)
+                min = n;
+
+            if (n % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        if (count == 0) {
+
+            System.out.println("ไม่มีข้อมูลตัวเลข");
+        } else {
+
+            double avg = (double) sum / count;
+
+            System.out.println("\n--- สรุปผล ---");
+            System.out.println("จำนวนตัวเลข: " + count + " ตัว");
+            System.out.println("ผลรวม: " + sum);
+            System.out.printf("ค่าเฉลี่ย: %.2f\n", avg);
+            System.out.println("Max: " + max);
+            System.out.println("Min: " + min);
+            System.out.println("เลขคู่: " + even + " ตัว");
+            System.out.println("เลขคี่: " + odd + " ตัว");
+        }
+    }
+}
